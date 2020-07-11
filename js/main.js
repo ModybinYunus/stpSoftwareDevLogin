4bffd9ebe3c6e26f993158e5e8905bd7a9bffedd
modybinyunus <modymas1997@gmail.com>
2020-07-11 12:12:43 +0200

    Stp SW Login Design

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var x = document.getElementById("signUp");
+var y = document.getElementById("signIn");
+var z = document.getElementById("mybtn");
+
+// Toggle Function 
+
+function signIn(){
+    x.style.left ="-600px";
+    y.style.left ="120px";
+    z.style.left ="0px";
+}
+function signUp(){
+    y.style.left ="-600px";
+    x.style.left ="120px";
+    z.style.left ="130px";
+}
