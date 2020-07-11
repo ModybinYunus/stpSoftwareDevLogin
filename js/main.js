bf9efe0c5514d6a531d9ba6489e5151627a71a1c
modybinyunus <modymas1997@gmail.com>
2020-07-11 22:56:07 +0200

    valid

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 var x = document.getElementById("signUp");
 var y = document.getElementById("signIn");
 var z = document.getElementById("mybtn");
+var m = document.getElementById("signUp_btn");
+var n = document.getElementById("signIn_btn");
 
 // Toggle Function 
 
@@ -14,3 +16,26 @@ function signUp(){
     x.style.left ="120px";
     z.style.left ="130px";
 }
+
+m.disabled = true;
+n.disabled =true;
+
+// Validation 
+// function isPasswordMatch() {
+//     var password = document.getElementById("pwd").value;
+//     var confirmPassword = document.getElementById("repwd").value;
+     
+//     if (password === confirmPassword) m.disabled = false;
+    
+//  }
+
+ function isPasswordMatch() {
+    var password = $("#pwd").val();
+    var confirmPassword = $("#repwd").val();
+    
+    if (password === confirmPassword) m.disabled = false;
+    }
+ $(document).ready(function () {
+    $("#repwd").keyup(isPasswordMatch);
+    }); 
+    
